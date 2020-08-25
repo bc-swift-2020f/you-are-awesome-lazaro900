0dcb03921781d588cd5b2f7586732db967452d2c
Lazaro Alvelaez <lazaroalvelaez@bc.edu>
2020-08-24 22:22:35 -0400

    Changed to this is BC

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Lazaro Alvelaez on 8/22/20.
+//  Copyright © 2020 Lazaro Alvelaez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
