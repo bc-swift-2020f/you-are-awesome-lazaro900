c3effeabe56e5a4bea22c9723b25077b14d3ddc1
Lazaro Alvelaez <lazaroalvelaez@bc.edu>
2020-09-05 16:44:52 -0400

    2.10 Finished

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,17 +12,51 @@ class ViewController: UIViewController {
     @IBOutlet weak var newnameLabel: UILabel!
     @IBOutlet weak var feelingImage: UIImageView!
     
+    // variables to control what message and image is shown
+    var imageNumber = 0
+    var messagenum = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        newnameLabel.text = ""
+        newnameLabel.text = "Welcome!"
         // Do any additional setup after loading the view.
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("You just pressed a button")
-        newnameLabel.text = "Good Job!"
-        feelingImage.image = UIImage(named: "image\(arc4random_uniform(9)).png")
+        
+        //this will control the image number and reset it when it reaches 10
+        print(imageNumber)
+        imageNumber = imageNumber + 1
+        if imageNumber == 10 {
+            imageNumber = 0
+        }
+        let imageName = "image\(imageNumber).png"
+        feelingImage.image = UIImage (named: imageName)
+        
+        
+        //these are the messages that will be displayed
+        let messages = ["You are Awesome!",
+                        "Good Job!",
+                        "Amazing",
+                        "You are Talented"]
+        
+        
+        //path for what message to display
+        if newnameLabel.text == messages[messagenum] {
+            newnameLabel.text = messages[messagenum]
+        } else if newnameLabel.text == messages[messagenum] {
+            newnameLabel.text = messages[messagenum]
+        }
+        else {
+            newnameLabel.text = messages[messagenum]
+        }
+        
+        //used to control what message is shown
+        messagenum += 1
+        if messagenum == 4{
+            messagenum = 0
+        }
 
         
     }
