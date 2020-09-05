ab9fa96328d2cb718c27b58ad1233a463e36f59e
Lazaro Alvelaez <lazaroalvelaez@bc.edu>
2020-09-05 18:12:17 -0400

    finished 2.11

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -25,38 +25,55 @@ class ViewController: UIViewController {
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         
-        //this will control the image number and reset it when it reaches 10
-        print(imageNumber)
-        imageNumber = imageNumber + 1
-        if imageNumber == 10 {
-            imageNumber = 0
-        }
-        let imageName = "image\(imageNumber).png"
+        //pick a random image
+        let imageName = "image\(Int.random(in: 0...9)).png"
         feelingImage.image = UIImage (named: imageName)
-        
+
         
         //these are the messages that will be displayed
         let messages = ["You are Awesome!",
                         "Good Job!",
                         "Amazing",
-                        "You are Talented"]
+                        "You are So Talented",
+                        "I Wish That I Was You",
+                        "You Have Mad Skillzzzzzz",
+                        "I Wanted To Let You Know How Amazing You Are"]
         
+        //pick a random message
+        newnameLabel.text = messages[Int.random(in: 0...messages.count-1)]
         
-        //path for what message to display
-        if newnameLabel.text == messages[messagenum] {
-            newnameLabel.text = messages[messagenum]
-        } else if newnameLabel.text == messages[messagenum] {
-            newnameLabel.text = messages[messagenum]
-        }
-        else {
-            newnameLabel.text = messages[messagenum]
-        }
-        
-        //used to control what message is shown
-        messagenum += 1
-        if messagenum == 4{
-            messagenum = 0
-        }
+//        //this will control the image number and reset it when it reaches 10
+//        print(imageNumber)
+//        imageNumber = imageNumber + 1
+//        if imageNumber == 10 {
+//            imageNumber = 0
+//        }
+//        let imageName = "image\(imageNumber).png"
+//        feelingImage.image = UIImage (named: imageName)
+//
+//
+//        //these are the messages that will be displayed
+//        let messages = ["You are Awesome!",
+//                        "Good Job!",
+//                        "Amazing",
+//                        "You are Talented"]
+//
+//
+//        //path for what message to display
+//        if newnameLabel.text == messages[messagenum] {
+//            newnameLabel.text = messages[messagenum]
+//        } else if newnameLabel.text == messages[messagenum] {
+//            newnameLabel.text = messages[messagenum]
+//        }
+//        else {
+//            newnameLabel.text = messages[messagenum]
+//        }
+//
+//        //used to control what message is shown
+//        messagenum += 1
+//        if messagenum == 4{
+//            messagenum = 0
+//        }
 
         
     }
